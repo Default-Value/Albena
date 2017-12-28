871b10238d501c0fae07e0cf1da38d67b7f6c40c
trevorsee <tcarr@mica.edu>
2017-12-27 23:33:28 -0600

    hovers

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,7 @@
-$(function() {
 
-	// Custom JS
+var height = document.body.clientHeight
+var target = document.querySelector('.header')
 
-});
+window.onscroll = function() {
+  target.style.opacity = 1 - (window.pageYOffset / height)
+}
