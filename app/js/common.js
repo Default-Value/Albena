9596e91d910587b1e66ed18930959ca06dd94264
trevorsee <tcarr@mica.edu>
2017-12-29 14:09:22 -0600

    tweaks

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,7 @@
 
-var height = document.body.clientHeight
-var target = document.querySelector('.header')
-
-window.onscroll = function() {
-  target.style.opacity = 1 - (window.pageYOffset / height)
-}
+// var height = document.body.clientHeight
+// var target = document.querySelector('.header')
+//
+// window.onscroll = function() {
+//   target.style.opacity = 1 - (window.pageYOffset / height)
+// }
